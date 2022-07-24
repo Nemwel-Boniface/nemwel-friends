dd0d0ef600e43f2566f5331eb87d527405eabe22
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-24 14:09:10 +0300

    Update included:   - Created the project my friends app   - Set up the project db using sqlite 3   - Setup the project's controllers and models   - Setup devise   - Setup the login/ out, signup functionalities

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  get 'home/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "home#index"
+
+  get 'home/about'
+end
