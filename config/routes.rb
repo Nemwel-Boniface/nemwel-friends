a14e052e4eade63e71fba2e6a98b74a9dbbc7ea4
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-24 18:00:51 +0300

    Update set up authorization

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  # root "home#index"
 
+  root "friends#index"
   get 'home/about'
 end
