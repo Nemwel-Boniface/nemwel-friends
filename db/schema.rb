539a66cee844bbe4302f38dec9bd5b0e70c41dc8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-07-24 14:31:34 +0300

    Update created a User id column in friends table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_093753) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_24_112158) do
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_24_093753) do
     t.string "twitter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_friends_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
